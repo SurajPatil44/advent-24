b4e11d7e1bfc2f1ae6855f52e60b25fec6478351
SurajPatil44 <suraj.patil716@gmail.com>
2025-05-09 18:37:03 +0530

    added solution 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -6,11 +6,61 @@ use utils::Content;
 Problem  : https://adventofcode.com/2024/day/1
 */
 
+fn lower_bound(sv: &Vec<u32>, n: u32) -> Option<usize> {
+    let mut l = 0;
+    let mut h = sv.len() - 1;
+    let mut ans: Option<usize> = None;
+    while l <= h {
+        let m = (l + h) / 2;
+
+        if sv[m] == n {
+            ans = Some(m);
+            h = m - 1;
+        } else if (sv[m] > n) {
+            l = m + 1;
+        } else {
+            h = m - 1;
+        }
+    }
+
+    return ans;
+}
+
+fn upper_bound(sv: &Vec<u32>, n: u32) -> Option<usize> {
+    let mut l = 0;
+    let mut h = sv.len() - 1;
+    let mut ans: Option<usize> = None;
+    while l <= h {
+        let m = (l + h) / 2;
+        //dbg!(m, sv[m]);
+        if sv[m] == n {
+            ans = Some(m);
+            l = m + 1;
+        } else if sv[m] > n {
+            l = m + 1;
+        } else {
+            h = m - 1;
+        }
+    }
+
+    return ans;
+}
+
+fn get_count(sv: &Vec<u32>, n: u32) -> usize {
+    let lb = lower_bound(sv, n);
+    //dbg!(lb);
+    if lb.is_none() {
+        return 0;
+    }
+    let rb = upper_bound(sv, n);
+    //dbg!(n, rb);
+    return rb.unwrap() - lb.unwrap() + 1;
+}
+
 fn main() {
     let mut args = std::env::args();
     let _pgm_name = args.next();
     let fname = args.next().unwrap_or("sample.txt".to_string());
-    let mut sum = 0;
     #[allow(unused_mut)]
     let mut reader = Content::read_from_file(&fname);
 
@@ -35,10 +85,21 @@ fn main() {
         let (i1, i2) = (list_1.pop().unwrap(), list_2.pop().unwrap());
         vector_list1.push(i1);
         vector_list2.push(i2);
+        //dbg!(i1, i2);
         distance += i1.abs_diff(i2);
     }
 
     println!("Solution 1 {}", distance);
 
+    //let vector_list1 = list_1.into_vec();
+    //let vector_list2 = list_2.into_vec();
     //assert_eq!(distance, 11);
+    //dbg!(&vector_list1, &vector_list2);
+    let mut distance2 = 0;
+    for num in vector_list1 {
+        let d = get_count(&vector_list2, num);
+        distance2 += (num * d as u32);
+    }
+
+    println!("Solution 2 {}", distance2);
 }
