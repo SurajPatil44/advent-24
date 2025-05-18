cdd8e816a8d2d91c0c32da291c2567a53bdee597
SurajPatil44 <suraj.patil716@gmail.com>
2025-05-18 23:04:35 +0530

    day 5 done

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,24 +1,44 @@
 use std::collections::{HashMap, HashSet};
 use utils::{partition, Content};
 
-fn check_record(records: &Vec<Vec<u32>>, index: usize, after: &HashMap<u32, HashSet<u32>>) -> bool {
-    let mut cpy: Vec<_> = records[index].iter().map(|x| x).collect();
+fn check_record(record: &Vec<u32>, after: &HashMap<u32, HashSet<u32>>) -> (bool, isize) {
+    let mut cpy: Vec<_> = record.iter().map(|x| x).collect();
     let mut banned: HashSet<u32> = HashSet::new();
     let mut prev = cpy.pop().unwrap();
+    let mut ind = record.len() - 1;
     if after.contains_key(&prev) {
         banned.extend(after[prev].clone());
     }
     while !cpy.is_empty() {
         let cur = cpy.pop().unwrap();
+        ind = ind - 1;
         if banned.contains(cur) {
-            return false;
+            return (false, ind as isize);
         }
         prev = cur;
         if after.contains_key(&prev) {
             banned.extend(after[prev].clone());
         }
     }
-    true
+    (true, -1)
+}
+
+fn fix_record(record: &Vec<u32>, after: &HashMap<u32, HashSet<u32>>, swap_ind: usize) -> u32 {
+    let mut cpy: Vec<_> = record.iter().map(|x| *x).collect();
+
+    cpy.swap(swap_ind, swap_ind + 1);
+
+    loop {
+        let (v, swap_ind) = check_record(&cpy, &after);
+        //dbg!(v, &cpy);
+        if v {
+            break;
+        }
+
+        cpy.swap(swap_ind as usize, swap_ind as usize + 1);
+    }
+
+    cpy[cpy.len() / 2]
 }
 fn main() {
     let mut args = std::env::args();
@@ -51,11 +71,16 @@ fn main() {
     }
     //dbg!(records);
     let mut ret = 0;
-    for i in 0..records.len() {
-        if check_record(&records, i, &after) {
-            let mid = records[i][records[i].len() / 2];
+    let mut fixed = 0;
+    for r in records {
+        let (v, i) = check_record(&r, &after);
+        if v {
+            let mid = r[r.len() / 2];
             ret += mid;
+        } else {
+            fixed += fix_record(&r, &after, i as usize);
         }
     }
     dbg!(ret);
+    dbg!(fixed);
 }
